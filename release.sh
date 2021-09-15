867e4d4c33775ebb9565492e1478e6175dfbba7e
Yeva Byzek <yeva@confluent.io>
2021-09-15 11:57:05 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -3,10 +3,3 @@
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "releasing branch $CURRENT_BRANCH to GH Pages"
 mkdocs gh-deploy
-git add site/.
-git commit -m "Deploy $CURRENT_BRANCH to site"
-git push origin master
-
-echo
-echo "Check site for latest:"
-echo "    https://confluentinc.github.io/ksqldb-recipes/"
