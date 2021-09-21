a47980495c623d68ec0996416fc6c353d0919887
Yeva Byzek <yeva@confluent.io>
2021-09-21 15:21:41 -0400

    Deployed 131fb2f with MkDocs version: 1.2.2

--- retail/inventory/source.sql
+++ retail/inventory/source.sql
@@ -1,19 +1,16 @@
--- Create stream of inventory
-CREATE STREAM inventory_stream (
-	id STRING key,
-	item STRING,
-	quantity INTEGER,
-	price DOUBLE)
-   with (VALUE_FORMAT='json',
-         KAFKA_TOPIC='inventory');
-
--- add some mock data
-insert into inventory_stream (id, item, quantity, price) values ('1', 'Apple Magic Mouse 2', 10, 99);
-insert into inventory_stream (id, item, quantity, price) values ('2', 'iPhoneX', 25, 999);
-insert into inventory_stream (id, item, quantity, price) values ('3', 'MacBookPro13', 100, 1799);
-insert into inventory_stream (id, item, quantity, price) values ('4', 'iPad4', 20, 340);
-insert into inventory_stream (id, item, quantity, price) values ('5', 'Apple Pencil', 10, 79);
-insert into inventory_stream (id, item, quantity, price) values ('5', 'PhoneX', 10, 899);
-insert into inventory_stream (id, item, quantity, price) values ('4', 'iPad4', -20, 399);
-insert into inventory_stream (id, item, quantity, price) values ('3', 'MacBookPro13', 10, 1899);
-insert into inventory_stream (id, item, quantity, price) values ('4', 'iPad4', 20, 399);
+CREATE SOURCE CONNECTOR inventory WITH (
+  'connector.class'          = 'PostgresSource',
+  'name'                     = 'confluent-postgresql-source',
+  'kafka.api.key'            = '<my-kafka-api-key>',
+  'kafka.api.secret'         = '<my-kafka-api-secret>',
+  'topic.prefix'             = 'postgresql_',
+  'connection.host'          = '<my-database-endpoint>',
+  'connection.port'          = '5432',
+  'connection.user'          = 'postgres',
+  'connection.password'      = '<my-database-password>',
+  'db.name'                  = 'postgres',
+  'table.whitelist'          = 'passengers',
+  'timestamp.column.name'    = 'created_at',
+  'output.data.format'       = 'JSON',
+  'db.timezone'              = 'UTC',
+  'tasks.max'                = '1');
