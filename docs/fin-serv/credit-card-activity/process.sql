f08335f9394b0d74e599ef7f58017a7f32844c10
Yeva Byzek <yeva@confluent.io>
2021-10-27 16:34:31 -0400

    Update names and statements per Colin

--- docs/fin-serv/credit-card-activity/process.sql
+++ docs/fin-serv/credit-card-activity/process.sql
@@ -1,5 +1,5 @@
 -- Register the stream of transactions
-CREATE STREAM TRANSACTIONS_RAW (
+CREATE STREAM FD_TRANSACTIONS_RAW (
   ACCOUNT_ID BIGINT, 
   TIMESTAMP VARCHAR, 
   CARD_TYPE VARCHAR, 
@@ -7,71 +7,71 @@ CREATE STREAM TRANSACTIONS_RAW (
   IP_ADDRESS VARCHAR, 
   TRANSACTION_ID VARCHAR
 ) WITH (
-  KAFKA_TOPIC='transactions',
+  KAFKA_TOPIC='FD_transactions',
   VALUE_FORMAT='JSON', 
   PARTITIONS=6
 );
 
 -- Repartition the stream on account_id in order to ensure that all the streams and tables are co-partitioned, which means that input records on both sides of the join have the same configuration settings for partitions.
-CREATE STREAM TRANSACTIONS_SOURCE AS
+CREATE STREAM FD_TRANSACTIONS_SOURCE AS
   SELECT * 
-  FROM TRANSACTIONS_RAW 
+  FROM FD_TRANSACTIONS_RAW 
   PARTITION BY ACCOUNT_ID;
 
 -- Register the existing stream of customer data
-CREATE STREAM CUST_RAW_STREAM (
+CREATE STREAM FD_CUST_RAW_STREAM (
   ID BIGINT, 
   FIRST_NAME VARCHAR, 
   LAST_NAME VARCHAR, 
   EMAIL VARCHAR, 
   AVG_CREDIT_SPEND DOUBLE
 ) WITH (
-  KAFKA_TOPIC='customers', 
+  KAFKA_TOPIC='FD_customers', 
   VALUE_FORMAT='JSON', 
   PARTITIONS=6
 );
 
 -- Repartition the customer data stream by account_id to prepare for the join
-CREATE STREAM CUSTOMER_REKEYED AS
+CREATE STREAM FD_CUSTOMER_REKEYED WITH (kafka_topic='FD_CUSTOMER_REKEYED') AS
   SELECT * 
-  FROM CUST_RAW_STREAM 
+  FROM FD_CUST_RAW_STREAM 
   PARTITION BY ID;
 
 -- Register the partitioned customer data topic as a table used for the join with the incoming stream of transactions:
-CREATE TABLE customer (
+CREATE TABLE FD_customer (
   ID BIGINT PRIMARY KEY,
   FIRST_NAME VARCHAR, 
   LAST_NAME VARCHAR, 
   EMAIL VARCHAR, 
   AVG_CREDIT_SPEND DOUBLE
 ) WITH (
-  KAFKA_TOPIC='CUSTOMER_REKEYED',
+  KAFKA_TOPIC='FD_CUSTOMER_REKEYED',
   VALUE_FORMAT='JSON',
   PARTITIONS=6
 );
 
 -- Join the transactions to customer information:
-CREATE STREAM TRANSACTIONS_ENRICHED AS 
+CREATE STREAM FD_TRANSACTIONS_ENRICHED AS 
   SELECT
     T.ACCOUNT_ID,
     T.CARD_TYPE,
     T.AMOUNT, 
     C.FIRST_NAME + ' ' + C.LAST_NAME AS FULL_NAME, 
     C.AVG_CREDIT_SPEND 
-  FROM TRANSACTIONS_SOURCE T 
+  FROM FD_TRANSACTIONS_SOURCE T 
   INNER JOIN CUSTOMER C 
   ON T.ACCOUNT_ID = C.ID;
 
 -- Aggregate the stream of transactions for each account ID using a two-hour tumbling window, and filter for accounts in which the total spend in a two-hour period is greater than the customer’s average:
-CREATE TABLE POSSIBLE_STOLEN_CARD AS 
+CREATE TABLE FD_POSSIBLE_STOLEN_CARD WITH (key_format='json') AS 
   SELECT
-    TIMESTAMPTOSTRING(WindowStart(), 'yyyy-MM-dd HH:mm:ss Z') AS WINDOW_START, 
+    TIMESTAMPTOSTRING(WINDOWSTART, 'yyyy-MM-dd HH:mm:ss Z') AS WINDOW_START, 
     T.ACCOUNT_ID,
     T.CARD_TYPE,
     SUM(T.AMOUNT) AS TOTAL_CREDIT_SPEND, 
     T.FULL_NAME,
     MAX(T.AVG_CREDIT_SPEND) AS AVG_CREDIT_SPEND 
-  FROM TRANSACTIONS_ENRICHED T 
+  FROM FD_TRANSACTIONS_ENRICHED T 
   WINDOW TUMBLING (SIZE 2 HOURS) 
   GROUP BY T.ACCOUNT_ID, T.CARD_TYPE, T.FULL_NAME 
   HAVING SUM(T.AMOUNT) > MAX(T.AVG_CREDIT_SPEND);
