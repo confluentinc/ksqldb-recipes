fc69d2bba4c48d7b14626934e4fa88428e55e971
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-11-30 13:22:38 -0600

    Monitor datacenter (#22)

---
+++ docs/operations/data-center/process.sql
@@ -0,0 +1,53 @@
+-- Create a Table for the captured tenant occupancy events
+CREATE TABLE tenant_occupancy (
+  tenant_id VARCHAR PRIMARY KEY,
+  customer_id BIGINT
+) WITH (
+  kafka_topic='tenant-occupancy',
+  partitions=3,
+  key_format='JSON',
+  value_format='JSON'
+);
+
+-- Create a Stream for the power control panel telemetry data.
+--   tenant_kwh_usage is reset by the device every month
+CREATE STREAM panel_power_readings (
+  panel_id BIGINT,
+  tenant_id VARCHAR,
+  panel_current_utilization DOUBLE,
+  tenant_kwh_usage BIGINT
+) WITH (
+  kafka_topic='panel-readings',
+  partitions=3,
+  key_format='JSON',
+  value_format='JSON'
+);
+
+-- Create a filtered Stream of panel readings registering power usage >= 85%
+--  good for determining panels which are drawing a high electrical load
+CREATE STREAM overloaded_panels AS 
+  SELECT panel_id, tenant_id, panel_current_utilization 
+    FROM panel_power_readings 
+    WHERE panel_current_utilization >= 0.85
+  EMIT CHANGES;
+
+-- Create a stream of billable power events 
+--  the tenant_kwh_usage field is the aggregate amount of power used in the
+--  current month 
+CREATE STREAM billable_power AS 
+  SELECT 
+      FORMAT_TIMESTAMP(FROM_UNIXTIME(panel_power_readings.ROWTIME), 'yyyy-MM') 
+        AS billable_month,
+      tenant_occupancy.customer_id as customer_id,
+      tenant_occupancy.tenant_id as tenant_id, 
+      panel_power_readings.tenant_kwh_usage as tenant_kwh_usage
+    FROM panel_power_readings
+    INNER JOIN tenant_occupancy ON 
+      panel_power_readings.tenant_id = tenant_occupancy.tenant_id
+  EMIT CHANGES;
+
+-- Create a table that can be queried for billing reports
+CREATE TABLE billable_power_report WITH (key_format='json') AS
+  SELECT customer_id, tenant_id, billable_month, MAX(tenant_kwh_usage) as kwh
+    FROM billable_power
+    GROUP BY tenant_id, customer_id, billable_month;
