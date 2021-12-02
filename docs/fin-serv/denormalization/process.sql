31a8a2eace696c730a2a6c7d5d26290ac8b5bc9d
Kris Jenkins <kjenkins@confluent.io>
2021-12-02 18:14:28 +0000

    Denormalization: Correcting a couple of SQL problems.

--- docs/fin-serv/denormalization/process.sql
+++ docs/fin-serv/denormalization/process.sql
@@ -3,7 +3,7 @@ CREATE STREAM orders (
   order_id BIGINT,
   customer_id BIGINT,
   item VARCHAR,
-  order_total_usd NUMERIC(10,2)
+  order_total_usd DECIMAL(10,2)
 ) WITH (
   KAFKA_TOPIC = 'orders',
   VALUE_FORMAT = 'json',
@@ -23,8 +23,11 @@ CREATE STREAM cust_raw_stream (
 );
 
 -- Register the customer data topic as a table
-CREATE TABLE customer (
-  id BIGINT PRIMARY KEY
+CREATE TABLE customers (
+  id BIGINT PRIMARY KEY,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  email VARCHAR
 ) WITH (
   KAFKA_TOPIC = 'CUST_RAW_STREAM',
   VALUE_FORMAT = 'JSON',
@@ -34,10 +37,11 @@ CREATE TABLE customer (
 -- Denormalize data: joining facts (orders) with the dimension (customer)
 CREATE STREAM orders_enriched AS
   SELECT
+    c.id AS customer_id,
     o.order_id AS order_id,
     o.item AS item,
     o.order_total_usd AS order_total_usd,
-    c.first_name || ' ' || C.last_name AS full_name,
+    CONCAT(CONCAT(c.first_name , ' ') , c.last_name) AS full_name,
     c.email AS email
   FROM orders o
     LEFT JOIN customers c
