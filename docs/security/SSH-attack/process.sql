045d0ec16cba5ca35ecc28c5b61057ea5d4c8ded
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-12-01 20:51:50 -0600

    improves the ssh recipe

--- docs/security/SSH-attack/process.sql
+++ docs/security/SSH-attack/process.sql
@@ -1,52 +1,44 @@
--- Define Kafka parameters
-DEFINE topic = 'syslog';
-
 -- Extract relevant fields from log messages
-CREATE OR REPLACE STREAM `syslog` WITH (
-  KAFKA_TOPIC = '${topic}',
+CREATE STREAM syslog (
+  ts varchar, 
+  host varchar,
+  facility int,
+  message varchar,
+  remote_address varchar 
+) WITH (
+  KAFKA_TOPIC = 'syslog',
   VALUE_FORMAT = 'json',
   PARTITIONS = 6
 );
 
-CREATE STREAM `by_facility` AS
-  SELECT
-    TIMESTAMPTOSTRING(TIMESTAMP, 'yyyy-MM-dd HH:mm:ss') AS SYSLOG_TIMESTAMP,
-    HOST,
-    FACILITY,
-    MESSAGE,
-    REMOTEADDRESS         
-  FROM `syslog`
-  PARTITION BY FACILITY
-  EMIT CHANGES;
-
 -- Flag events with invalid users, and enrich with a new field 'FACILITY_DESCRIPTION'
-CREATE STREAM `invalid_users` AS
+CREATE STREAM invalid_users AS
   SELECT
-    SYSLOG_TIMESTAMP,
-    HOST,
-    FACILITY,
-    MESSAGE, 
-    REMOTEADDRESS,
-    CASE WHEN FACILITY = 0 THEN 'kernel messages'
-         WHEN FACILITY = 1 THEN 'user-level messages'
-         WHEN FACILITY = 2 THEN 'mail system'
-         WHEN FACILITY = 3 THEN 'system daemons'
-         WHEN FACILITY = 4 THEN 'security/authorization messages'
-         WHEN FACILITY = 5 THEN 'messages generated internally by syslogd'
-         WHEN FACILITY = 6 THEN 'line printer subsystem'
+    FORMAT_TIMESTAMP(ts, 'yyyy-MM-dd HH:mm:ss') AS syslog_timestamp,
+    host,
+    facility,
+    message, 
+    remote_address,
+    CASE WHEN facility = 0 THEN 'kernel messages'
+         WHEN facility = 1 THEN 'user-level messages'
+         WHEN facility = 2 THEN 'mail system'
+         WHEN facility = 3 THEN 'system daemons'
+         WHEN facility = 4 THEN 'security/authorization messages'
+         WHEN facility = 5 THEN 'messages generated internally by syslogd'
+         WHEN facility = 6 THEN 'line printer subsystem'
          ELSE '<unknown>'
-       END AS FACILITY_DESCRIPTION
-  FROM `by_facility`
-  WHERE MESSAGE LIKE 'Invalid user%'
+       END AS facility_description
+  FROM syslog 
+  WHERE message LIKE 'Invalid user%'
   EMIT CHANGES;
 
 -- Create actionable stream of SSH attacks, enriched with user and IP
-CREATE STREAM `ssh_attacks` AS 
+CREATE STREAM ssh_attacks AS 
   SELECT
-    SYSLOG_TIMESTAMP,
-    HOST,
-    FACILITY_DESCRIPTION,
-    SPLIT(REPLACE(MESSAGE,'Invalid user ',''),' from ')[1] AS ATTACK_USER,
-    SPLIT(REPLACE(MESSAGE,'Invalid user ',''),' from ')[2] AS ATTACK_IP
-  FROM `invalid_users`
+    syslog_timestamp,
+    host,
+    facility_description,
+    SPLIT(REPLACE(message, 'Invalid user ', ''), ' from ')[1] AS attack_user,
+    remote_address AS attack_ip
+  FROM invalid_users
   EMIT CHANGES;
