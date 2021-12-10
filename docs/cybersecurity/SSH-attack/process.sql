3a3f305e826294c60b677a1fce793e9d59d29942
Yeva Byzek <yeva@confluent.io>
2021-12-10 13:16:50 -0500

    Streamline SSH attack recipe #86

--- docs/cybersecurity/SSH-attack/process.sql
+++ docs/cybersecurity/SSH-attack/process.sql
@@ -11,14 +11,13 @@ CREATE STREAM syslog (
   PARTITIONS = 6
 );
 
--- Flag events with invalid users, and enrich with a new field 'FACILITY_DESCRIPTION'
-CREATE STREAM invalid_users AS
+-- Create actionable stream of SSH attacks, filtering syslog messages where user is invalid,
+-- and enriched with user and IP
+CREATE STREAM ssh_attacks AS
   SELECT
     FORMAT_TIMESTAMP(ts, 'yyyy-MM-dd HH:mm:ss') AS syslog_timestamp,
     host,
     facility,
-    message, 
-    remote_address,
     CASE WHEN facility = 0 THEN 'kernel messages'
          WHEN facility = 1 THEN 'user-level messages'
          WHEN facility = 2 THEN 'mail system'
@@ -27,18 +26,9 @@ CREATE STREAM invalid_users AS
          WHEN facility = 5 THEN 'messages generated internally by syslogd'
          WHEN facility = 6 THEN 'line printer subsystem'
          ELSE '<unknown>'
-       END AS facility_description
-  FROM syslog 
-  WHERE message LIKE 'Invalid user%'
-  EMIT CHANGES;
-
--- Create actionable stream of SSH attacks, enriched with user and IP
-CREATE STREAM ssh_attacks AS 
-  SELECT
-    syslog_timestamp,
-    host,
-    facility_description,
+       END AS facility_description,
     SPLIT(REPLACE(message, 'Invalid user ', ''), ' from ')[1] AS attack_user,
     remote_address AS attack_ip
-  FROM invalid_users
+  FROM syslog
+  WHERE message LIKE 'Invalid user%'
   EMIT CHANGES;
