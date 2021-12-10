3956d21219097ac9666f6d49e222968e2c64abdb
Yeva Byzek <yeva@confluent.io>
2021-12-10 08:29:21 -0500

    #84: optimize credit card recipe

--- docs/anomaly-detection/credit-card-activity/process.sql
+++ docs/anomaly-detection/credit-card-activity/process.sql
@@ -1,5 +1,5 @@
 -- Register the stream of transactions
-CREATE STREAM FD_TRANSACTIONS_RAW (
+CREATE STREAM FD_TRANSACTIONS (
   ACCOUNT_ID BIGINT, 
   TIMESTAMP VARCHAR, 
   CARD_TYPE VARCHAR, 
@@ -12,15 +12,7 @@ CREATE STREAM FD_TRANSACTIONS_RAW (
   PARTITIONS=6
 );
 
--- Repartition the stream on account_id in order to ensure that all the streams
--- and tables are co-partitioned, which means that input records on both sides
--- of the join have the same configuration settings for partitions.
-CREATE STREAM FD_TRANSACTIONS_SOURCE AS
-  SELECT * 
-  FROM FD_TRANSACTIONS_RAW 
-  PARTITION BY ACCOUNT_ID;
-
--- Register the existing stream of customer data
+-- Register the stream of customer data
 CREATE STREAM FD_CUST_RAW_STREAM (
   ID BIGINT, 
   FIRST_NAME VARCHAR, 
@@ -39,9 +31,9 @@ CREATE STREAM FD_CUSTOMER_REKEYED WITH (KAFKA_TOPIC='FD_CUSTOMER_REKEYED') AS
   FROM FD_CUST_RAW_STREAM 
   PARTITION BY ID;
 
--- Register the partitioned customer data topic as a table used for the join
--- with the incoming stream of transactions:
-CREATE TABLE FD_customer (
+-- Register the partitioned customer data topic as a table
+-- used for the join with the stream of transactions:
+CREATE TABLE FD_CUSTOMERS (
   ID BIGINT PRIMARY KEY,
   FIRST_NAME VARCHAR, 
   LAST_NAME VARCHAR, 
@@ -53,22 +45,22 @@ CREATE TABLE FD_customer (
   PARTITIONS=6
 );
 
--- Join the transactions to customer information:
-CREATE STREAM FD_TRANSACTIONS_ENRICHED AS 
+-- Join the transactions to customer information
+CREATE STREAM FD_TRANSACTIONS_ENRICHED WITH (KAFKA_TOPIC='transactions_enriched') AS
   SELECT
     T.ACCOUNT_ID,
     T.CARD_TYPE,
     T.AMOUNT, 
     C.FIRST_NAME + ' ' + C.LAST_NAME AS FULL_NAME, 
     C.AVG_CREDIT_SPEND 
-  FROM FD_TRANSACTIONS_SOURCE T 
-  INNER JOIN FD_CUSTOMER C 
+  FROM FD_TRANSACTIONS T
+  INNER JOIN FD_CUSTOMERS C
   ON T.ACCOUNT_ID = C.ID;
 
 -- Aggregate the stream of transactions for each account ID using a two-hour
 -- tumbling window, and filter for accounts in which the total spend in a
 -- two-hour period is greater than the customer’s average:
-CREATE TABLE FD_POSSIBLE_STOLEN_CARD WITH (KEY_FORMAT='json') AS 
+CREATE TABLE FD_POSSIBLE_STOLEN_CARD WITH (KAFKA_TOPIC='possible_stolen_card',KEY_FORMAT='json') AS
   SELECT
     TIMESTAMPTOSTRING(WINDOWSTART, 'yyyy-MM-dd HH:mm:ss Z') AS WINDOW_START, 
     T.ACCOUNT_ID,
@@ -76,7 +68,7 @@ CREATE TABLE FD_POSSIBLE_STOLEN_CARD WITH (KEY_FORMAT='json') AS
     SUM(T.AMOUNT) AS TOTAL_CREDIT_SPEND, 
     T.FULL_NAME,
     MAX(T.AVG_CREDIT_SPEND) AS AVG_CREDIT_SPEND 
-  FROM FD_TRANSACTIONS_ENRICHED T 
+  FROM FD_TRANSACTIONS_ENRICHED T
   WINDOW TUMBLING (SIZE 2 HOURS) 
   GROUP BY T.ACCOUNT_ID, T.CARD_TYPE, T.FULL_NAME 
   HAVING SUM(T.AMOUNT) > MAX(T.AVG_CREDIT_SPEND);
