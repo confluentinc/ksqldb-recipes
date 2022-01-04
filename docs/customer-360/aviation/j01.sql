9ad9648bb85c995e472f1797b011ea6d14ed8d26
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-01-04 13:44:01 -0500

    #116: Aviation recipe is missing process.sql (#117)

--- docs/customer-360/aviation/j01.sql
+++ docs/customer-360/aviation/j01.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customer_bookings AS 
   SELECT C.*, B.ID, B.FLIGHT_ID
   FROM   bookings B
-          INNER JOIN CUSTOMERS C
+          INNER JOIN customers C
               ON B.CUSTOMER_ID = C.ID;
