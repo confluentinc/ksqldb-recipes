4633f03ba2ca0f6087d086f6e83d1a3b49030f79
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2022-01-11 17:33:43 -0600

    WIP: format manual command

--- docs/cybersecurity/ddos/manual.sql
+++ docs/cybersecurity/ddos/manual.sql
@@ -1,3 +1,14 @@
-INSERT INTO network_traffic VALUES (
-  
+INSERT INTO network_traffic (TIMESTAMP, layers)
+VALUES (
+  UNIX_TIMESTAMP(), 
+  STRUCT(
+    frame := STRUCT(time  := 'time', protocols := 'protocols'),
+    eth   := STRUCT(src   := 'src',  dst       := 'dst'),
+    ip    := STRUCT(src   := 'src',  src_host  := 'src_host',
+                    dst   := 'dst',  dst_host  := 'dst_host',
+                    proto := 'proto'),
+    tcp   := STRUCT(srcport   := 'srcport',  dstport     := 'dstport',
+                    flags_ack := 'flags_ack',flags_reset := 'flags_reset')
+  )
 );
+  
