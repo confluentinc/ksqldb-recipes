184e5c6d5d129689213dd002a61f6c546621ab6b
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2022-01-13 14:24:16 -0600

    fixes some formatting on ddos, create connector WIP

--- docs/cybersecurity/ddos/process.sql
+++ docs/cybersecurity/ddos/process.sql
@@ -1,7 +1,6 @@
 SET 'auto.offset.reset' = 'earliest';
 
-CREATE STREAM network_traffic
-(
+CREATE STREAM network_traffic (
   timestamp BIGINT,
   layers STRUCT<
    frame STRUCT< 
@@ -21,12 +20,11 @@ CREATE STREAM network_traffic
       dstport VARCHAR, 
       flags_ack VARCHAR, 
       flags_reset VARCHAR >>
-)
-WITH (
-  KAFKA_TOPIC='network-traffic', 
-  TIMESTAMP='timestamp', 
-  VALUE_FORMAT='JSON', 
-  PARTITIONS=6
+) WITH (
+  KAFKA_TOPIC = 'network-traffic', 
+  TIMESTAMP = 'timestamp', 
+  VALUE_FORMAT = 'JSON', 
+  PARTITIONS = 6
 );
 
 
@@ -34,8 +32,10 @@ CREATE TABLE potential_slowloris_attacks AS
 SELECT 
   layers->ip->src, count(*) as count_connection_reset
 FROM network_traffic 
-WINDOW TUMBLING (SIZE 60 SECONDS)
+  WINDOW TUMBLING (SIZE 60 SECONDS)
 WHERE 
   layers->tcp->flags_ack = '1' AND layers->tcp->flags_reset = '1'
-GROUP BY layers->ip->src
-HAVING count(*) > 10;
+GROUP BY
+  layers->ip->src
+HAVING 
+  count(*) > 10;
