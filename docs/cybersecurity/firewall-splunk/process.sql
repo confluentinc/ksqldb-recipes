d50104db275097a31ea7db1bf36eea82a5a46008
Yeva Byzek <yeva@confluent.io>
2022-01-13 09:32:38 -0500

    Optimize SQL

--- docs/cybersecurity/firewall-splunk/process.sql
+++ docs/cybersecurity/firewall-splunk/process.sql
@@ -15,9 +15,12 @@ CREATE STREAM splunk (
 
 -- Split message into array
 CREATE STREAM splunk_parsed WITH (KAFKA_TOPIC = 'splunk_parsed') AS SELECT
-  *, 
+  time,
+  host,
+  sourcetype,
   REGEXP_SPLIT_TO_ARRAY(event, ' ')
 FROM splunk
+WHERE sourcetype = 'cisco:asa'
 EMIT CHANGES;
 
 -- Filter messages for Cisco ASA and where action is Deny
@@ -32,5 +35,5 @@ CREATE STREAM splunk_filtered WITH (KAFKA_TOPIC = 'splunk_filtered') AS SELECT
   SPLIT(REGEXP_SPLIT_TO_ARRAY(KSQL_COL_0[7], 'outside:')[2], '/')[1] as outside_ip,
   SPLIT(REGEXP_SPLIT_TO_ARRAY(KSQL_COL_0[7], 'outside:')[2], '/')[2] as outside_port
 FROM splunk_parsed
-WHERE sourcetype = 'cisco:asa' and KSQL_COL_0[2] = 'Deny'
+WHERE KSQL_COL_0[2] = 'Deny'
 EMIT CHANGES;
