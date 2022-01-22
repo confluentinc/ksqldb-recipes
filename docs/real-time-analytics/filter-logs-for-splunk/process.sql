b77ec941d984e68c74cb55742884b6cec1fd0b0c
Bill Bejeck <bbejeck@gmail.com>
2022-01-21 19:41:24 -0500

    Resovle merge conflicts

---
+++ docs/real-time-analytics/filter-logs-for-splunk/process.sql
@@ -0,0 +1,50 @@
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE STREAM audit_log_events (
+ log_events STRUCT< 
+            id VARCHAR, 
+            source VARCHAR, 
+            specversion VARCHAR, 
+            type VARCHAR, 
+            time VARCHAR,  
+            datacontenttype VARCHAR, 
+            subject VARCHAR, 
+            confluentRouting STRUCT<
+                route VARCHAR >,	
+			data STRUCT<
+			    serviceName VARCHAR, 
+			    methodName VARCHAR, 
+			    resourceName VARCHAR, 
+			    authenticationInfo STRUCT<
+			        principal VARCHAR>, 
+			    authorizationInfo STRUCT<
+			        granted BOOLEAN,
+			        operation VARCHAR,
+			        resourceType VARCHAR,
+			        resourceName VARCHAR,
+			        patternType VARCHAR,
+			        superUserAuthorization BOOLEAN>,
+			    request STRUCT<
+			        correlation_id VARCHAR,
+			        client_id VARCHAR>,
+			    requestMetadata STRUCT<
+                    client_address VARCHAR>>>
+) WITH (
+	 KAFKA_TOPIC = 'confluent-audit-log-events', 
+	 VALUE_FORMAT='JSON', 
+	 PARTITIONS = 6
+);
+
+
+-- Application logic
+CREATE STREAM audit_log_topics
+WITH (
+	KAFKA_TOPIC='topic-operations-audit-log', 
+	TIMESTAMP='time', 
+	TIMESTAMP_FORMAT='yyyy-MM-dd''T''HH:mm:ss.SSSX',
+	PARTITIONS=6
+) 
+AS SELECT LOG_EVENTS->time as time, LOG_EVENTS->DATA
+	FROM  audit_log_events
+	WHERE (LOG_EVENTS->DATA->AUTHORIZATIONINFO->RESOURCETYPE = 'Topic')
+EMIT CHANGES;
