f669398159f5a1b9d434e3c2320689bb0c81e392
Danica Fine <dfine@confluent.io>
2022-01-25 14:29:42 -0800

    two sources

--- docs/real-time-analytics/logistics/source.sql
+++ docs/real-time-analytics/logistics/source.sql
@@ -1,6 +1,6 @@
-CREATE SOURCE CONNECTOR fleet_updates WITH (
+CREATE SOURCE CONNECTOR orders WITH (
   'connector.class'          = 'PostgresSource',
-  'name'                     = 'recipe-postgres-logistics',
+  'name'                     = 'recipe-postgres-logistics-orders',
   'kafka.api.key'            = '<my-kafka-api-key>',
   'kafka.api.secret'         = '<my-kafka-api-secret>',
   'connection.host'          = '<my-database-endpoint>',
@@ -8,7 +8,24 @@ CREATE SOURCE CONNECTOR fleet_updates WITH (
   'connection.user'          = 'postgres',
   'connection.password'      = '<my-database-password>',
   'db.name'                  = '<db-name>',
-  'table.whitelist'          = 'fleet_updates, orders',
+  'table.whitelist'          = 'orders',
+  'timestamp.column.name'    = 'timestamp',
+  'output.data.format'       = 'JSON',
+  'db.timezone'              = 'UTC',
+  'tasks.max'                = '1'
+);
+
+CREATE SOURCE CONNECTOR fleet_updates WITH (
+  'connector.class'          = 'PostgresSource',
+  'name'                     = 'recipe-postgres-logistics-fleet',
+  'kafka.api.key'            = '<my-kafka-api-key>',
+  'kafka.api.secret'         = '<my-kafka-api-secret>',
+  'connection.host'          = '<my-database-endpoint>',
+  'connection.port'          = '5432',
+  'connection.user'          = 'postgres',
+  'connection.password'      = '<my-database-password>',
+  'db.name'                  = '<db-name>',
+  'table.whitelist'          = 'fleet_updates',
   'timestamp.column.name'    = 'timestamp',
   'output.data.format'       = 'JSON',
   'db.timezone'              = 'UTC',
