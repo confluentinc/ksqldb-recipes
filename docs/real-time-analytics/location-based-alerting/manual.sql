a081f808c84c9bea9e1ca07f51a175993bb8f4bb
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2022-01-26 12:40:40 -0600

    location based: unique-ify merchant data

--- docs/real-time-analytics/location-based-alerting/manual.sql
+++ docs/real-time-analytics/location-based-alerting/manual.sql
@@ -1,5 +1,5 @@
 INSERT INTO MERCHANT_LOCATIONS (id, latitude, longitude, description, geohash) VALUES (1, 14.5486606, 121.0477211, '7-Eleven RCBC Center', 'wdw4f88206fx');
-INSERT INTO MERCHANT_LOCATIONS (id, latitude, longitude, description, geohash) VALUES (2, 14.5473328, 121.0516176, '7-Eleven South of Market', 'wdw4f87075kt');
+INSERT INTO MERCHANT_LOCATIONS (id, latitude, longitude, description, geohash) VALUES (2, 14.5473328, 121.0516176, 'Jordan Manila', 'wdw4f87075kt');
 INSERT INTO MERCHANT_LOCATIONS (id, latitude, longitude, description, geohash) VALUES (3, 14.5529666, 121.0516716, 'Lawson Eco Tower', 'wdw4f971hmsv');
 
 INSERT INTO USER_LOCATIONS (id, latitude, longitude, geohash) VALUES (1, 14.5472791, 121.0475401, 'wdw4f820h17g');
