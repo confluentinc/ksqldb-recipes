a051ebdc1785bf8344f30161b7d9bd6689b4a25a
Yeva Byzek <yeva@confluent.io>
2022-01-25 20:42:23 -0500

    Add PARTITION BY; replace latest_by_offset with CREATE SOURCE TABLE

--- docs/real-time-analytics/mainframe-offload/process.sql
+++ docs/real-time-analytics/mainframe-offload/process.sql
@@ -21,17 +21,17 @@ CREATE STREAM mq_transactions_normalized WITH (KAFKA_TOPIC = 'mq_transactions_no
     UNIX_TIMESTAMP() AS ts_cache,
     (UNIX_TIMESTAMP() - timestamp) AS ts_delta
 FROM mq_transactions
+PARTITION BY dep_account_no
 EMIT CHANGES;
 
--- Materialize the stream of transactions into a table,
--- like a local cache, that reflects the latest for each dep_account_no
-CREATE TABLE mq_cache WITH (KAFKA_TOPIC = 'mq_cache')
-  AS SELECT
-    dep_account_no,
-    latest_by_offset(BALANCE) AS balance_latest,
-    latest_by_offset(ts_stream) AS ts_stream_latest,
-    latest_by_offset(ts_cache) AS ts_cache_latest,
-    latest_by_offset(ts_delta) AS ts_delta_latest
-FROM mq_transactions_normalized
-GROUP BY dep_account_no
-EMIT CHANGES;
+CREATE SOURCE TABLE mq_cache (
+    dep_account_no STRING PRIMARY KEY,
+    balance BIGINT,
+    ts_stream BIGINT,
+    ts_cache BIGINT,
+    ts_delta BIGINT
+) WITH (
+    KAFKA_TOPIC='mq_transactions_normalized',
+    KEY_FORMAT='KAFKA',
+    VALUE_FORMAT='JSON'
+);
